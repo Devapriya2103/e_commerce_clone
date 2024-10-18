8aa8697ca7edd0ef6609f3c810b73e00fadfd726
Devapriya2103 <syamkumardevapriya@gmail.com>
2024-10-18 16:00:10 +0530

    first commit

---
+++ lib/utils/image_constants.dart
@@ -0,0 +1,3 @@
+class ImageConstants {
+  static const MYAPPLOGO = "assets/images/Group 34010.png";
+}
---
+++ lib/view/splash_screen/splash_screen.dart
@@ -0,0 +1,57 @@
+import 'dart:async';
+
+import 'package:e_commerce_clone/utils/color_constants.dart';
+import 'package:e_commerce_clone/utils/image_constants.dart';
+import 'package:e_commerce_clone/view/onboarding_screen/onboarding_screen.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+    @override
+  void initState() {
+    Timer(Duration(seconds: 3), () {
+        Navigator.pushReplacement(
+        context, 
+        MaterialPageRoute(
+            builder: (context) => OnboardingScreen(),
+            ),
+        );
+    }
+    );
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                    Image.asset(
+                      ImageConstants.MYAPPLOGO,
+                    height: 100,
+                    width: 125,
+                    fit: BoxFit.fill,
+                    ),
+                    SizedBox(width: 9),
+                    Text("Stylish",
+                    style: TextStyle(
+                        color: ColorConstants.PRIMARY,
+                        fontSize: 40,
+                        fontWeight: FontWeight.bold,
+                    ),
+                    ),
+                ],
+            ),
+        ),
+    );
+  }
+}
